a4f6102cf14b4212ead2b76025e1ff657058cb30
aicreatorsg <aicreatorabhi1001@gmail.com>
2024-07-25 02:05:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask server is running!'
+    return 'Flask server is running!' 
 
 @app.route('/check_connectivity')
 def check_connectivity():
